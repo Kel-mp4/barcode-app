dc7d64ede9d80a05366400f7a565db1859379c20
Kel-mp4 <118085988+Kel-mp4@users.noreply.github.com>
2025-11-23 21:01:31 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
 const generatorTab = document.querySelector(".nav-gene");
 const scannerTab = document.querySelector(".nav-scan");
-const inventoryTab = document.querySelector(".nav-inv");
+
 
 generatorTab.addEventListener("click", () => {
   generatorTab.classList.add("active");
   scannerTab.classList.remove("active");
-  inventoryTab.classList.remove("active");
 
-  document.querySelector(".inventory").style.display = "none";
   document.querySelector(".scanner").style.display = "none";
   document.querySelector(".generator").style.display = "block";
 })
@@ -15,19 +13,9 @@ generatorTab.addEventListener("click", () => {
 scannerTab.addEventListener("click", () => {
   scannerTab.classList.add("active");
   generatorTab.classList.remove("active");
-   inventoryTab.classList.remove("active");
-
-   document.querySelector(".inventory").style.display = "none";
+  
   document.querySelector(".scanner").style.display = "block";
   document.querySelector(".generator").style.display = "none";
 })
 
-inventoryTab.addEventListener("click", () => {
-  scannerTab.classList.remove("active");
-  generatorTab.classList.remove("active");
-   inventoryTab.classList.add("active");
 
-   document.querySelector(".inventory").style.display = "block";
-  document.querySelector(".scanner").style.display = "none";
-  document.querySelector(".generator").style.display = "none";
-})
